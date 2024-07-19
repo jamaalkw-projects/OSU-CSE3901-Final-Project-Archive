dd97eb32814386f57d6ed0a80123d5586c0d0b67
jamaalkw <wairegi.3@buckeyemail.osu.edu>
2024-07-19 13:21:33 -0400

    Created test for quiz_controller

--- test/controllers/quiz_controller_test.rb
+++ test/controllers/quiz_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class QuizControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # Created 07/19/24 by Jamaal Wairegi
+  # Test 1 - Quiz Index Page
+  test "should get Quizzes page" do
+    get quizzes_url
+    assert_response :success
+  end
 end
