4210b0fc8d7c0d2387627363fe6bce9c64a5733f
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-07-24 14:32:33 -0400

    setting up the room

--- app/models/room.rb
+++
@@ -1,7 +0,0 @@
-class Room < ApplicationRecord
-  belongs_to :quiz
-
-  validates_uniqueness_of :name
-  validates :name, presence: true, length: { minimum: 6, maximum: 10 }, numericality: { only_integer: true }
-  scope :public_rooms, -> { where(is_private: false) }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_154449) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_182803) do
   create_table "correct_choices", force: :cascade do |t|
     t.string "option"
     t.integer "question_id", null: false
@@ -45,6 +45,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_154449) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_quizzes_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "room_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
