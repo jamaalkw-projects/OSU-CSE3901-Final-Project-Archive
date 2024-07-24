c93301614806c9732ae40c663d84f2c5dbe9d89c
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-07-24 11:59:57 -0400

    Updated documentation and validating quiz before saving

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,4 @@
 class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
+  validates :quiz_name, presence: true, length: {minimum: 8, message: 'must be at least 8 chars long.'}
 end
