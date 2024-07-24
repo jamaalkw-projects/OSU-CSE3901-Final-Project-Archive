5cec72c6f31227a39b19a846fd6a1f559bd1e10d
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-07-24 12:52:28 -0400

    Setting up environment to load dynamically as db changes. Most changes are from the gems.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_003639) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_160558) do
   create_table "correct_choices", force: :cascade do |t|
     t.string "option"
     t.integer "question_id", null: false
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_003639) do
 
   create_table "quizzes", force: :cascade do |t|
     t.string "quiz_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
