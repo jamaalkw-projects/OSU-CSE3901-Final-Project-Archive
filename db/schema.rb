849fa393e1a67cf2f81aa84bc3aef7aca1c6989e
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-07-25 11:15:17 -0400

    Updated RoomUser fixed minor html issue in home.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_233338) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_25_142900) do
   create_table "correct_choices", force: :cascade do |t|
     t.string "option"
     t.integer "question_id", null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_233338) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.integer "score"
     t.index ["room_id"], name: "index_room_users_on_room_id"
     t.index ["user_id"], name: "index_room_users_on_user_id"
   end
